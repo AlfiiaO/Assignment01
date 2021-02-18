cb1c2093bde318fa6e3b0c9cefefbf23716f6263
AlfiiaO <ellie5768@gmail.com>
2021-02-17 16:48:34 -0800

    init comit

---
+++ js/script.js
@@ -0,0 +1,142 @@
+// STEP 1
+let someMonth;
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+/* function theMonth(); */
+
+// STEP 2
+
+ 5, 2.5 , 30 // Numeric Literal Expression
+ "Hello", 'JavaScript', " "            // String Literal Expression 
+ true , false // Boolean Literal Expression 
+ null // Null Literal Expression
+
+ // STEP 3
+ let complexExpression = (8 + 4) * 7;
+ let expressionVariable = 3 * (12 / 5) + ( 6 + 4);
+
+ // STEP 4 
+
+let firstName; 
+let lastName;
+let address;
+let city;
+let state;
+let zipCode;
+let yourAge;
+let referralSource;
+let mayWecontactYou; 
+
+// STEP 5 
+
+//  Declare and Assign Variable
+
+const PI = 3.14159; // constant can not be changed after it was declared.
+let yourName = "James";
+let zCode = 92101;
+/* let yourName = "James" , zCode = 92101; */ // another way to declare variable
+
+// STEP 6
+
+let val = 10 +  '10';
+console.log(val);
+
+// STEP 7 
+let myNumber = true + "String";
+console.log(myNumber);
+
+let store = 28 + false;
+console.log(store);
+
+//STEP 8  This string is not valid because "string" is a reserved keyword. I changed the name and also added \' for  a single quote.
+ 
+let someText = 'Who once said,"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someText);
+
+// STEP 9 
+
+let z = null;
+window.console.log(z);
+
+let carModel;
+window.console.log(carModel);
+
+// STEP 10 
+console.log(typeof "JavaScript"); // return String
+
+console.log(typeof 88 ); // return Number
+
+console.log(typeof true); // return Boolean
+
+console.log(typeof undefined); // return Undefined
+
+// STEP 11 
+
+let a = "Hello Alfiia Osipova,";
+let b = "welcome to the JavaScript class!";
+alert(a + " " + b); 
+
+// STEP 12 
+
+let myName = "Alfiia";
+alert(myName);
+
+// STEP 13 
+
+let course = "JavaScript";
+alert(course);
+
+// STEP 14
+
+let c = "Hello Alfiia Osipova.\nWelcome to the JavaScript class!";
+alert(c);
+
+//STEP 15
+
+let person = prompt("What is your name?" ,"");
+alert(person);
+
+// STEP 16
+
+let jsCourse = prompt("What class are you taking?" ,"")
+alert(jsCourse);
+
+// STEP 17
+
+let x = 10;
+let y = 20;
+console.log(x + y)
+
+// STEP 18
+
+x = 20;
+console.log(x + 20);
+
+// STEP 19 
+ x = 20;
+ console.log(x * 5);
+
+ // STEP  20
+
+ x = 20 % 3;
+ console.log(x * 1);
+
+ // STEP 21
+ let f = 5;
+ let p = 10;
+ console.log(f < p); 
+
+ x = 5
+ y = 5
+ console.log(x == 5 && y == 5 );
+
+ // STEP 22 
+
+ f = 20;
+ p = 35;
+ console.log(f >= p);
+
+ x = 18;
+ y = 23;
+ console.log( x > 30 || y > 25);
